73ee7017399f2c34aa44226f4193162813ce5632
Muhammad12134 <mahammad.ohonov@yandex.kz>
2025-06-26 07:06:30 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
-from aiogram.client.default import DefaultBotProperties  # добавлено правильно
+from aiogram.client.default import DefaultBotProperties
 from config import BOT_TOKEN
 from handlers import commands, signals
 
@@ -14,9 +14,19 @@ dp = Dispatcher()
 dp.include_router(commands.router)
 dp.include_router(signals.router)
 
+WEBHOOK_PATH = "/webhook"
+WEBHOOK_PORT = 8080
+WEBHOOK_URL = f"https://jarvistradex-bot.onrender.com{WEBHOOK_PATH}"
+
 async def main():
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    await bot.set_webhook(WEBHOOK_URL)
+    await dp.start_webhook(
+        webhook_path=WEBHOOK_PATH,
+        bot=bot,
+        skip_updates=True,
+        host="0.0.0.0",
+        port=WEBHOOK_PORT
+    )
 
 if __name__ == "__main__":
     asyncio.run(main())
